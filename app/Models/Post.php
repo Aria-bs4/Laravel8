04a5bdea96ed4d0f5794fe9d5999333617187e69
aria_m <aria_bs4@yahoo.com>
2022-08-04 14:22:38 +0430

    RouteBindModel : lession 23

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -11,4 +11,9 @@ class Post extends Model
 
     protected $guarded = []; // all columns allow mass assigne
     // protected $fillable = ['title', 'excerpt','body'];
+
+    // public function getRouteKeyName()
+    // {
+    //     return 'slug';
+    // }
 }
