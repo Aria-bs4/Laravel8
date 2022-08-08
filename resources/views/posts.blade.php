f9035e72a1f206e1519e15de8f367ff287ecd176
aria_m <aria_bs4@yahoo.com>
2022-08-08 19:50:53 +0430

    convert css html, use template style and dive into componentss, lesson 32

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,25 +1,11 @@
-
 <x-layout>
-    <x-slot name="content">
-        @foreach($posts as $post)
-        <article class="{{ $loop->even ? 'foobar' : '' }}">
-            <h1>
-                <a href="/posts/{{ $post->slug }}">
-                    {{ $post->title }}
-                </a>
-            </h1>
+    @include ('_post-header')
 
-            <p>
-                By <a href="/authors/{{ $post->author->username }}">{{ $post->author->name }}</a> in <a href='/categories/{{ $post->category->slug }}'>{{ $post->category->name }}</a>
-            </p>
-
-            <div>{{ $post->excerpt }}</div>
-        </article>
-        @endforeach
-    </x-slot>
-
-    <x-slot name="title">
-        My Blog
-    </x-slot>
-
+    <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+        @if ($posts->count())
+            <x-post-grid :posts="$posts" />
+        @else
+            <p class="text-center">There is no post yet. Try again later</p>
+        @endif
+    </main>
 </x-layout>
