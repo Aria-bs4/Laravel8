07342e7e44e956e85532c011fb37c8977d8c938d
aria_m <aria_bs4@yahoo.com>
2022-08-12 20:55:48 +0430

    messy search in posts, lesson 37

--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,16 @@ use Spatie\YamlFrontMatter\YamlFrontMatter;
 */
 
 Route::get('/', function () {
+    $posts = Post::latest();
+
+    if (request('search')) {
+        $posts
+            ->where('title', 'like', '%'.request('search').'%')
+            ->orwhere('body', 'like', '%'.request('search').'%');
+    }
+
     return view('posts', [
-        'posts' => Post::with(['category' => fn($query) => $query->where('id','>',0)])->latest()->get(),
+        'posts' => $posts->get(),
         'categories' => Category::all()
     ]);
 })->name('home');
