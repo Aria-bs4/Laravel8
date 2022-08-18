f6cdd23b71ab9372d3bfdfa1df5d7b9d73d0b430
aria_m <aria_bs4@yahoo.com>
2022-08-18 17:18:13 +0430

    merge category and search request, lesson 42

--- resources/views/components/category-dropdown.blade.php
+++ resources/views/components/category-dropdown.blade.php
@@ -9,11 +9,11 @@
             </button>
         </x-slot>
 
-        <x-dropdown-item href="/" :active="request()->routeIs('home')">All</x-dropdown-item>
+        <x-dropdown-item href="/" :active=" !request('category')">All</x-dropdown-item>
 
         @foreach ($categories as $category)
             <x-dropdown-item 
-                href="?category={{ $category->slug }}"
+                href="?category={{ $category->slug }}&{{ http_build_query(request()->except('category')) }}"
                 :active="$category->is($currentCategory)"
             >{{ ucwords($category->name) }}</x-dropdown-item>
         @endforeach
