bb0cef8303e43118508f1f2fde09c4f4c7a6a89b
aria_m <aria_bs4@yahoo.com>
2022-08-21 00:30:00 +0430

    add more validation and learn error,lesson 47

--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -15,8 +15,13 @@
                         type="text"
                         name="name"
                         id="name"
+                        value="{{ old('name')}}"
                         required
                     >
+
+                    @error('name')
+                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
                 </div>
 
                 <div class="mb-6">
@@ -28,8 +33,13 @@
                         type="text"
                         name="username"
                         id="username"
+                        value="{{ old('username')}}"
                         required
                     >
+
+                    @error('username')
+                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
                 </div>
 
                 <div class="mb-6">
@@ -41,8 +51,13 @@
                         type="email"
                         name="email"
                         id="email"
+                        value="{{ old('email')}}"
                         required
                     >
+
+                    @error('email')
+                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
                 </div>
 
                 <div class="mb-6">
@@ -56,6 +71,10 @@
                         id="password"
                         required
                     >
+
+                    @error('password')
+                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
                 </div>
 
                 <div class="mb-6">
@@ -65,6 +84,14 @@
                         Submit
                     </button>
                 </div>
+
+                @if ($errors->any())
+                    <ul>    
+                    @foreach ($errors->all() as $error)
+                        <li class="text-red-500 text-xs">{{ $error }}</li>
+                    @endforeach
+                    </ul>
+                @endif
             </form>
         </main>
     </section>
