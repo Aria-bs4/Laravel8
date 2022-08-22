4f831c063fd7f43838b41fc50a2d33e53628676f
aria_m <aria_bs4@yahoo.com>
2022-08-22 22:31:29 +0430

    add login page, lesson 50

--- app/Http/Controllers/SessionController.php
+++ app/Http/Controllers/SessionController.php
@@ -2,8 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Validation\ValidationException;
+
 class SessionController extends Controller
 {
+    public function create(){
+        return view('session.create');
+    }
+
+    public function store(){
+        $attributes = request()->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+        
+        if (auth()->attempt($attributes)) {
+            return redirect('/')->with('success', 'Welcome Back!');
+        }
+
+        throw ValidationException::withMessages([
+            'email' => 'Your provided credentials could not be verified'
+        ]);
+
+        // return back()
+        //     ->withInput()
+        //     ->withErrors(['email' => 'Your provided credentials could not be verified']);
+
+
+    }
+
     public function destroy() {
         auth()->logout();
 
