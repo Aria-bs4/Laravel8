8a749968bc587bfaba7346b5c6442306323e9d50
aria_m <aria_bs4@yahoo.com>
2022-08-24 23:23:19 +0430

    make comments dynamicly, lesson 54

--- resources/views/components/post-comment.blade.php
+++ resources/views/components/post-comment.blade.php
@@ -1,18 +1,18 @@
+@props(['comment'])
+
 <article class="bg-gray-100 border border-gray-200 flex p-6 rounded-xl space-x-4">
     <div class="flex-shrink-0">
-        <img src='https://i.pravatar.cc/60' with="60" height="60" class="rounded-xl">
+        <img src='https://i.pravatar.cc/60?u={{ $comment->id }}' with="60" height="60" class="rounded-xl">
     </div>
 
     <div>
         <header class="mb-4">
-            <h3>John Dou</h3>
+            <h3>{{ $comment->author->username }}</h3>
             <p class="text-xs">
                 posted
-                <time>8 month ago</time>
+                <time>{{ $comment->created_at }}</time>
             </p>
         </header>
-        <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure
-        </p>
+        {{ $comment->body }}
     </div>
 </article>
