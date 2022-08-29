9ecb017dcedd26d28f9476416114ed58d0fc3232
aria_m <aria_bs4@yahoo.com>
2022-08-29 23:56:19 +0430

    submit emails and show error or success, lesson 59

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,9 @@ use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('ping', function() {
+Route::post('newsletter', function() {
+    request()->validate(['email' => "required|email"]);
+
     $mailchimp = new \MailchimpMarketing\ApiClient();
 
     $mailchimp->setConfig([
@@ -14,12 +16,18 @@ Route::get('ping', function() {
         'server' => 'us10'
     ]);
 
-    $response = $mailchimp->lists->addListMember('cd8fbbfdfe',[
-        "email_address" => "Lindsey.White93@hotmail.com",
+    try{
+    $mailchimp->lists->addListMember('cd8fbbfdfe',[
+        "email_address" => request('email'),
         "status" => "subscribed",
     ]);
+    } catch (\Exception $e) {
+        throw \Illuminate\Validation\ValidationException::withMessages([
+            'email' => 'This email could not be added.'
+        ]);
+    }
 
-    dd($response);
+    return redirect('/')->with('success', 'You are now signed up for request!');
 });
 
 
