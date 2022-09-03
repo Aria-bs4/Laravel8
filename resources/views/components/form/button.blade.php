3a96918bcc813ecdf7c1748ea5e44061fd6909de
aria_m <aria_bs4@yahoo.com>
2022-09-03 21:38:18 +0430

    make form as blade component, lesson 65

---
+++ resources/views/components/form/button.blade.php
@@ -0,0 +1,5 @@
+<x-form.field>
+    <button type="submit" class="bg-blue-500 px-10 py-2 rounded-2xl sumbit text-white text-xs uppercase font-semibold hover:bg-blue-600">
+        {{ $slot }}
+    </button>
+</x-form.field>
--- resources/views/components/submit-button.blade.php
+++
@@ -1,3 +0,0 @@
-<button type="submit" class="bg-blue-500 px-10 py-2 rounded-2xl sumbit text-white text-xs uppercase font-semibold hover:bg-blue-600">
-    {{ $slot }}
-</button>
